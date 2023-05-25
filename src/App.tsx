0f360accb6a5dda3f879f983e3e73eeebf58a5a0
Archi999999 <archi999999@gmail.com>
2023-05-26 00:20:01 +0300

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,40 @@ import './App.css';
 import {Counter} from './components/Counter';
 import {CounterSetting} from './components/CounterSetting';
 import {Button} from "./components/Button";
-import {countReducer} from "./components/countReducer";
+import {
+    changeMaxValueAC,
+    changeStartValueAC,
+    countReducer,
+    incrementAC,
+    resetAC,
+    settingAC
+} from "./components/countReducer";
+
+
+export type InitialStateType = {
+    currentValue: number
+    startValue: number
+    maxValue: number
+    disabledInc: boolean
+    disabledRes: boolean
+    disabledSet: boolean
+    error: string
+}
+
+const initialState = {
+    currentValue: 0,
+    startValue: 0,
+    maxValue: 0,
+    disabledInc: true,
+    disabledRes: true,
+    disabledSet: false,
+    error: ''
+}
 
 function App() {
 
-    const initialState = {
-        values: {
-            currentValue: 0,
-            startValue: 0,
-            maxValue: 0
-        },
-        disabledInc: true,
-        disabledRes: true,
-        disabledSet: false,
-        error: ''
-    }
 
-    const [state, dispatch] = useReducer(countReducer, initialState)
+    const [state, dispatchState] = useReducer(countReducer, initialState)
 
     // const [currentValue, setCurrentValue] = useState(0)
     // const [startValue, setStartValue] = useState(0)
@@ -57,76 +74,81 @@ function App() {
     // }, [])
 
     const increment = () => {
-        setCurrentValue(currentValue + 1)
-        if (currentValue === maxValue - 1) {
-            setDisabledInc(true)
-            setError('max value')
-        }
-        localStorage.setItem('count', JSON.stringify(currentValue+ 1))
+        dispatchState(incrementAC())
+        //     setCurrentValue(currentValue + 1)
+        //     if (currentValue === maxValue - 1) {
+        //         setDisabledInc(true)
+        //         setError('max value')
+        //     }
+        //     // localStorage.setItem('count', JSON.stringify(currentValue+ 1))
     }
 
     const reset = () => {
-        setDisabledInc(false)
-        setError('')
-        setCurrentValue(startValue)
+        dispatchState(resetAC())
+        // setDisabledInc(false)
+        // setError('')
+        // setCurrentValue(startValue)
     }
 
     //--------------------------------Setting-------------------------------//
 
     const setting = () => {
+        dispatchState(settingAC())
         // savedStartValue = startValue
-        setDisabledSet(true)
-        setCurrentValue(startValue)
-        setDisabledInc(false)
-        setDisabledRes(false)
-        setError('')
-        localStorage.setItem('keyCounter', JSON.stringify([maxValue, startValue]))
+        // setDisabledSet(true)
+        // setCurrentValue(startValue)
+        // setDisabledInc(false)
+        // setDisabledRes(false)
+        // setError('')
+        // localStorage.setItem('keyCounter', JSON.stringify([maxValue, startValue]))
     }
 
     const changeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
-        if (Number(e.currentTarget.value) <= startValue) {
-            setDisabledInc(true)
-            setDisabledRes(true)
-            setDisabledSet(true)
-            setError('incorrect value')
-        } else if (Number(e.currentTarget.value) !== maxValue) {
-            setDisabledInc(true)
-            setDisabledRes(true)
-            setDisabledSet(false)
-            setError("enter values and press 'set'")
-        } else {
-            setDisabledSet(false)
-            setError('')
-        }
-        setMaxValue(Number(e.currentTarget.value))
+        dispatchState(changeMaxValueAC(+e.currentTarget.value))
+        // if (Number(e.currentTarget.value) <= startValue) {
+        //     setDisabledInc(true)
+        //     setDisabledRes(true)
+        //     setDisabledSet(true)
+        //     setError('incorrect value')
+        // } else if (Number(e.currentTarget.value) !== maxValue) {
+        //     setDisabledInc(true)
+        //     setDisabledRes(true)
+        //     setDisabledSet(false)
+        //     setError("enter values and press 'set'")
+        // } else {
+        //     setDisabledSet(false)
+        //     setError('')
+        // }
+        // setMaxValue(Number(e.currentTarget.value))
     }
 
     const changeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
-        if (Number(e.currentTarget.value) < 0 || Number(e.currentTarget.value) >= maxValue) {
-            setDisabledInc(true)
-            setDisabledRes(true)
-            setDisabledSet(true)
-            setError('incorrect value')
-        } else if (Number(e.currentTarget.value) !== startValue) {
-            setDisabledInc(true)
-            setDisabledRes(true)
-            setDisabledSet(false)
-            setError("enter values and press 'set'")
-        } else {
-            setDisabledSet(false)
-            setError('')
-        }
-        setStartValue(Number(e.currentTarget.value))
+        dispatchState(changeStartValueAC(+e.currentTarget.value))
+        // if (Number(e.currentTarget.value) < 0 || Number(e.currentTarget.value) >= maxValue) {
+        //     setDisabledInc(true)
+        //     setDisabledRes(true)
+        //     setDisabledSet(true)
+        //     setError('incorrect value')
+        // } else if (Number(e.currentTarget.value) !== startValue) {
+        //     setDisabledInc(true)
+        //     setDisabledRes(true)
+        //     setDisabledSet(false)
+        //     setError("enter values and press 'set'")
+        // } else {
+        //     setDisabledSet(false)
+        //     setError('')
+        // }
+        // setStartValue(Number(e.currentTarget.value))
     }
 
     return (
         <div className="App">
-            <CounterSetting setting={setting} maxValue={maxValue} startValue={startValue}
+            <CounterSetting setting={setting} maxValue={state.maxValue} startValue={state.startValue}
                             changeStartValue={changeStartValue} changeMaxValue={changeMaxValue}
-                            disabledSet={disabledSet}
+                            disabledSet={state.disabledSet}
             />
-            <Counter value={currentValue} increment={increment} reset={reset} disabledInc={disabledInc}
-                     disabledRes={disabledRes} error={error}/>
+            <Counter value={state.currentValue} increment={increment} reset={reset} disabledInc={state.disabledInc}
+                     disabledRes={state.disabledRes} error={state.error}/>
         </div>
     );
 }
--- src/components/countReducer.ts
+++ src/components/countReducer.ts
@@ -1,21 +1,89 @@
-export const countReducer =(state:any, action:ActionsType)=>{
-    const [currentValue, startValue, maxValue] = state.values
-    const [disabledInc, disabledRes, disabledSet, error] = state
-    switch (action.type){
-        case 'INCREMENT':{
-            (currentValue === maxValue - 1) ? {...state, }
-            // return {...state, currentValue: state.currentValue+1}
+import {InitialStateType} from "../App";
+
+export const countReducer = (state: InitialStateType, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'INCREMENT': {
+            return state.currentValue === (state.maxValue - 1)
+                ? {...state, currentValue: state.currentValue + 1, disabledInc: true, error: 'max value'}
+                : {...state, currentValue: state.currentValue + 1}
         }
-        default: return state
+        case 'RESET' : {
+            return {
+                ...state, disabledInc: false, error: "", currentValue: state.startValue
+            }
+        }
+        case "SETTING": {
+            return {
+                ...state, disabledSet: true, currentValue: state.startValue,
+                disabledInc: false, disabledRes: false, error: ''
+            }
+        }
+        case "CHANGE_MAX_VALUE": {
+            if (action.value <= state.startValue) {
+                return {...state, disabledInc: true, disabledRes: true, disabledSet: true, error: 'incorrect value', maxValue: action.value}
+            } else if (action.value !== state.maxValue) {
+                return {
+                    ...state,
+                    disabledSet: false,
+                    disabledRes: true,
+                    disabledInc: true,
+                    error: "enter values and press 'set'", maxValue: action.value
+                }
+            } else {
+                return {...state, disabledSet: false, error: '', maxValue: action.value}
+            }
+        }
+        case "CHANGE_START_VALUE": {
+            if (action.value < 0 || action.value >= state.maxValue) {
+                return {...state, disabledInc:true, disabledRes:true, disabledSet:true, error: 'incorrect value', startValue: action.value}
+            } else if (action.value !== state.startValue) {
+                return {...state, disabledInc:true, disabledRes: true, disabledSet: false, error: "enter values and press 'set'", startValue: action.value}
+            } else {return {...state, disabledSet:false, error: '', startValue: action.value}
+            }
+        }
+        default:
+            return state
+
     }
 
 }
 
-type ActionsType = ReturnType<typeof incrementAC>
+type ActionsType =
+    ReturnType<typeof incrementAC>
+    | ReturnType<typeof resetAC>
+    | ReturnType<typeof settingAC>
+    | ReturnType<typeof changeMaxValueAC>
+    | ReturnType<typeof changeStartValueAC>
 
 
-export const incrementAC =()=> {
+export const incrementAC = () => {
     return {
         type: 'INCREMENT'
-    }as const
+    } as const
+}
+
+export const resetAC = () => {
+    return {
+        type: 'RESET'
+    } as const
+}
+
+export const settingAC = () => {
+    return {
+        type: 'SETTING'
+    } as const
+}
+
+export const changeMaxValueAC = (value: number) => {
+    return {
+        type: 'CHANGE_MAX_VALUE',
+        value
+    } as const
+}
+
+export const changeStartValueAC = (value: number) => {
+    return {
+        type: 'CHANGE_START_VALUE',
+        value
+    } as const
 }
